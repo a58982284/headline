7b81d66fbbda432b7d23eb76410c27c352b026f5
a58982284 <a58982284@163.com>
2018-09-15 21:28:43 +0800

    1.01
    修改了keyerror显示错误的bug

--- headlines.py
+++ headlines.py
@@ -11,15 +11,25 @@ from flask import Flask,render_template,request,make_response
 
 app = Flask(__name__)
 
-RSS_FEED ={
+
+def rss_feed(publication):
+    RSS_FEED ={
 "zhihu": "https://www.zhihu.com/rss",
-"netease": "http://news.163.com/special/00011K6L/rss_newsattitude.xml",
 "songshuhui": "http://songshuhui.net/feed",
 "ifeng": "http://news.ifeng.com/rss/index.xml",
 "guojixinwen":"http://news.qq.com/newsgj/rss_newswj.xml",
 "guoneixinwen":"http://news.qq.com/newsgn/rss_newsgn.xml",
 "lishi":"http://news.qq.com/histor/rss_history.xml",
 }
+    #print('publication is',publication)
+    #print(type(publication))
+    if str(publication) in RSS_FEED.keys():
+
+        return RSS_FEED[publication]
+    else:
+        return RSS_FEED["songshuhui"]
+
+
 
 DEFAULTS = {
 'city': '北京',
@@ -38,11 +48,11 @@ def get_value_with_fallback(key,*args,**kwargs):
     if request.args.get(key):
         return request.args.get(key)
     if request.cookies.get(key):
-        print('request.cookies')
-        print(request.cookies)
+        #print('request.cookies')
+        #print(request.cookies)
         #{'Pycharm-bdfc5fce': '44e1e6e2-1007-463a-9c3d-891072768e2e', 'session': 'eyJfcGVybWFuZW50Ijp0cnVlLCJ1c2VyX2lkIjpudWxsfQ.Dn3_kQ.Z__XTqr0WsANIF21pwva2qpNHyo', 'city': '广州', 'publication': 'guoneixinwen'}
-        print('key')
-        print(key)  #city
+        #print('key')
+        #print(key)  #city
         return request.cookies.get(key)
     #else:
         #return DEFAULTS[key]
@@ -50,8 +60,17 @@ def get_value_with_fallback(key,*args,**kwargs):
 
 def get_news(publication):
     # feedparser解析RSS
-    feed = feedparser.parse(RSS_FEED[publication])
+    #print('RSS_FEED is',RSS_FEED[publication])
+    #if RSS_FEED[publication] is not None:
+    #feed = feedparser.parse(RSS_FEED[publication])
+    feed = feedparser.parse(rss_feed(publication))
+    print ("feed['entries']is",feed['entries'])
     return feed['entries']
+    #else:
+        #return feedparser.parse(RSS_FEED['guojixinwen'])
+
+    #print ('feed is',feed)
+    #return feed['entries']
 
 def get_weather(city):
     code = WEATHERS.get(city,101010100)
@@ -67,10 +86,12 @@ def get_weather(city):
 @app.route('/')
 def home():
     publication = get_value_with_fallback('publication')
+    print ('publication is',publication)
     city = get_value_with_fallback('city')
 
     weather = get_weather(city)
     articles = get_news(publication)
+    print ('articles is',articles)
 
     response = make_response(render_template('home.html',articles=articles,
                                              weather=weather))
